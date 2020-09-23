78a8059c1d176299d5535f57f480d4163864ae23
Peter Parikka <peter.parikka@frantic.com>
2020-09-23 19:48:00 +0300

    Move favorite state to item level

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { ReactComponent as FavoriteLogo } from "../assets/icons/favorite.svg";
 import "./List.scss";
 
-const List = ({ songs, favorites, onFavoriteClick }) => {
+const List = ({ songs }) => {
+  const [favorites, setFavorites] = useState([]);
+  const [favorite, setFavorite] = useState({
+    id: "",
+    songId: "",
+    favorite: false,
+  });
+  const [errors, setErrors] = useState();
+
+  useEffect(() => {
+    const removeFavorite = async () => {
+      await axios
+        .delete(`/favorites/${favorite.id}`)
+        .then((response) => fetchFavorites())
+        .catch((error) => setErrors(error));
+    };
+    const addFavorite = async () => {
+      await axios
+        .post(`/favorites`, { songId: favorite.songId })
+        .then((response) => fetchFavorites())
+        .catch((error) => setErrors(error));
+    };
+
+    if (favorite.songId && !favorite.favorite) {
+      addFavorite();
+    } else if (favorite.id && favorite.favorite) {
+      removeFavorite();
+    }
+
+    const fetchFavorites = async () => {
+      const results = await fetch("/favorites", {
+        headers: { accept: "application/json" },
+      });
+      const data = await results.json();
+      setFavorites(data);
+    };
+    fetchFavorites();
+  }, [favorite]);
+
   const checkForFav = (id) => {
     const fav = favorites.find((fav) => fav.songId === id);
     return {
       favoriteId: fav && fav.id ? fav.id : null,
       favorite: !!fav ? true : false,
-      songId: id
+      songId: id,
     };
   };
-  const addFavoritesToSongs = songs.map(song => {
-    return {...song, ...checkForFav(song.id)}
-  })
+  const addFavoritesToSongs = songs.map((song) => {
+    return { ...song, ...checkForFav(song.id) };
+  });
 
   return (
     <ul className="list">
@@ -21,12 +60,18 @@ const List = ({ songs, favorites, onFavoriteClick }) => {
         return (
           <li key={item.id}>
             {item.title}
-            <button onClick={(e) => onFavoriteClick(item.favoriteId, item.favorite, item.songId)}>
+            <button
+              onClick={(e) =>
+                setFavorite({
+                  id: item.favoriteId,
+                  songId: item.songId,
+                  favorite: item.favorite,
+                })
+              }
+            >
               <FavoriteLogo
                 className={
-                  item.favorite
-                    ? "favorite--active"
-                    : "favorite--notactive"
+                  item.favorite ? "favorite--active" : "favorite--notactive"
                 }
               />
             </button>
--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import List from "./List";
 
 const ListContainer = ({ query }) => {
   const [songs, setSongs] = useState([]);
-  const [favorite, setFavorite] = useState({
-    id: "",
-    songId: '',
-    favorite: false,
-  });
-  const [favorites, setFavorites] = useState([]);
+
   const [errors, setErrors] = useState();
   // console.log(songs);
   const sleep = (ms) => {
@@ -31,14 +25,14 @@ const ListContainer = ({ query }) => {
     const loadSongs = async () => {
       if (!query) {
         const songs = await getSongs(query, controller);
-        return setSongs(songs, favorites);
+        return setSongs(songs);
       }
 
       await sleep(350);
 
       if (currentQuery) {
         const songs = await getSongs(query, controller);
-        setSongs(songs, favorites);
+        setSongs(songs);
       }
     };
     loadSongs();
@@ -47,44 +41,16 @@ const ListContainer = ({ query }) => {
       currentQuery = false;
       controller.abort();
     };
-  }, [favorites, query]);
+  }, [query]);
 
-  useEffect(() => {
-    const removeFavorite = async () => {
-      await axios
-        .delete(`/favorites/${favorite.id}`)
-        .then((response) => fetchFavorites())
-        .catch((error) => setErrors(error));
-    };
-    const addFavorite = async () => {
-      await axios
-        .post(`/favorites`, { songId: favorite.songId })
-        .then((response) => fetchFavorites())
-        .catch((error) => setErrors(error));
-    };
-    console.log(favorite)
-    if (favorite.songId && !favorite.favorite) {
-      addFavorite();
-    } else if (favorite.id && favorite.favorite) {
-      removeFavorite();
-    }
 
-    const fetchFavorites = async () => {
-      const results = await fetch("/favorites", {
-        headers: { accept: "application/json" },
-      });
-      const data = await results.json();
-      setFavorites(data);
-    };
-    fetchFavorites();
-  }, [favorite]);
 
   return (
     <div>
       <List
         songs={songs}
-        favorites={favorites}
-        onFavoriteClick={(id, favorite, songId) => setFavorite({ id, songId, favorite })}
+        // favorites={favorites}
+        // onFavoriteClick={(id, favorite, songId) => setFavorite({ id, songId, favorite })}
       />
     </div>
   );
